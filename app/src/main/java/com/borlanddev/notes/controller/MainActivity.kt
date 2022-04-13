25b9a00cab09626e5b894e1e39d3c4765cabb742
BorlandDev <minsk1115596@gmail.com>
2022-04-13 22:53:03 +0300

    Added ToolBar to Navigation

--- app/src/main/java/com/borlanddev/notes/controller/MainActivity.kt
+++ app/src/main/java/com/borlanddev/notes/controller/MainActivity.kt
@@ -1,20 +1,58 @@
 package com.borlanddev.notes.controller
 
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.window.SplashScreen
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NamedNavArgument
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.recyclerview.widget.RecyclerView
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.borlanddev.notes.R
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration : AppBarConfiguration
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = (supportFragmentManager.findFragmentById(R.id.navHostFragment)
+                as NavHostFragment).navController
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        setupActionBarWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+
+            when (destination.id) {
+
+                R.id.splashFragment -> supportActionBar?.hide()
+              /*R.id.ListFragment -> Проверять первый ли это запуск приложения .
+              Показывать анимацию и вызвать функцию прослушки сети. Если запуск не первый, показывать
+              анимацию под тулбаром и также слушать сеть.
+              */
+
+              //R.id.DetailsFragment ->  продолжать показывать анимацию
+
+                else -> supportActionBar?.show()
+            }
+        }
+
 
     }
+
+
+
 
 }
